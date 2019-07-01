e4993c73b1c622a692c8311d508ea7af90cb21f6
youngeffort <893520964@qq.com>
2019-07-01 10:21:46 +0800

    mod：优化代码

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <div id="app">
-    <cyUploadImage
-
-    />
+    <cyUploadImage />
   </div>
 </template>
 
@@ -19,7 +17,7 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
